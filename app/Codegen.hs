97145dc9ea74ec19b5206995b3e758e65f371fc2
Abhiroop Sarkar <asiamgenius@gmail.com>
2018-11-29 21:51:52 +0000

    Define the top LLVM monad which constructs the state

--- app/Codegen.hs
+++ app/Codegen.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Codegen where
 
 import Control.Monad.State
-
 import LLVM.AST
 
+import qualified LLVM.AST as AST
+import qualified Data.ByteString.Short as BS
 import qualified Data.Map as Map
+import qualified Data.String.UTF8 as U
 
 double :: Type
 double = FloatingPointType DoubleFP
@@ -34,3 +37,18 @@ type Names = Map.Map String Int
 
 newtype Codegen a = Codegen { runCodegen :: State CodegenState a }
   deriving (Functor, Applicative, Monad, MonadState CodegenState)
+
+newtype LLVM a = LLVM (State AST.Module a)
+  deriving (Functor, Applicative, Monad, MonadState AST.Module)
+
+runLLVM :: AST.Module -> LLVM a -> AST.Module
+runLLVM mod (LLVM s) = execState s mod
+
+emptyModule :: [Char] -> AST.Module
+emptyModule label
+  = defaultModule { moduleName = (BS.pack . U.encode) label }
+
+addDefinition :: Definition -> LLVM ()
+addDefinition d = do
+  defs <- gets moduleDefinitions
+  modify $ \s -> s { moduleDefinitions = defs ++ [d] }
